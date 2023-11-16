199856c476fb505565667858b7bf58ce7c62925c
Bong <2021bongwonny@gmail.com>
2023-11-17 02:03:23 +0900

    refactor : modify styling method to style-jsx
    - modify page.tsx to client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+"use client"
+
 import Image from 'next/image';
-import styles from './page.module.scss';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className='main'>
       <Image
         src='/space_uchu_penguin.png'
         alt='space penguin'
@@ -11,12 +12,62 @@ export default function Home() {
         height={300}
         priority
       />
-      <h1 className={'mb5'}>최고의 한 줄</h1>
-      <h3 className={'mb10'}>~릴레이 소설 쓰기~</h3>
-      <p className={'text_center mb20'}>
+      <h1 className='mb5'>최고의 한 줄</h1>
+      <h3 className='mb10'>~릴레이 소설 쓰기~</h3>
+      <p className='text_center mb20'>
         오늘은 내가 소설가 ! <br /> 릴레이 소설 쓰기에 참여해보세요.
       </p>
-      <button className={styles.button}>참여하러가기</button>
+      <button className='button'>참여하러가기</button>
+      
+      <style jsx>{`
+        .main {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 6rem;
+          min-height: 100vh;
+        }
+
+        .description {
+          text-align: center;
+        }
+
+        .button {
+          padding: 10px 15px;
+          border-radius: 10px;
+          font-size: 1rem;
+          background-color: #1d4ed8;
+          font-weight: 700;
+
+          &:hover {
+            background-color: #3b82f6;
+          }
+
+          &:active {
+            background-color: #1e3a8a;
+          }
+        }
+
+        /* Next.js template CSS */
+
+        /* Mobile */
+        .template {
+          border-bottom: 1px solid rgba(var(--callout-border-rgb), 0.25);
+          background: linear-gradient(
+            to bottom,
+            rgba(var(--background-start-rgb), 1),
+            rgba(var(--callout-rgb), 0.5)
+          );
+          background-clip: padding-box;
+          backdrop-filter: blur(24px);
+        }
+
+        /* Tablet and Smaller Desktop */
+
+        @media (prefers-color-scheme: dark) {
+        }
+
+      `}</style>
     </main>
   );
 }
