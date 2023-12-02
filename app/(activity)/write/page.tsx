a376bfbc80e70527941964c0fa1005067ac012fd
Bong <2021bongwonny@gmail.com>
2023-12-02 18:40:58 +0900

    feat : add code which sending data to server
    - install axios
    - add rewrites in next.config

--- app/(activity)/write/page.tsx
+++ app/(activity)/write/page.tsx
@@ -1,6 +1,29 @@
+'use client';
+import { ChangeEvent, useState } from 'react';
 import styles from './page.module.scss';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
 
 export default function Write() {
+  const router = useRouter();
+  const [content, setContent] = useState('');
+  const handleWriteContent = (e: ChangeEvent) => {
+    const target = e.target as HTMLInputElement;
+    setContent(target.value);
+  };
+  const handleSendMessage = () => {
+    if (content) {
+      alert(content); // test code
+      axios
+        .post('/server/write', { text: content })
+        .then((res) => {
+          router.push('/write/complete');
+        })
+        .catch((err) => alert('작성에 실패하였습니다.'));
+    } else {
+      alert('내용을 작성해주세요!');
+    }
+  };
   const lines = [
     '"뭘 하고 계시는 거예요?"',
     '"술 마시지."',
@@ -21,14 +44,20 @@ export default function Write() {
         지금까지 이렇게 이야기가 진행되고 있어요.
       </p>
       <p className={styles.information_text}>다음에 올 내용을 적어주세요.</p>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={(e) => e.preventDefault()}>
         <input
           className={styles.input}
           name='content'
           id='input'
           placeholder='여기에 내용이 이어지도록 작성해보세요.'
+          value={content}
+          onChange={handleWriteContent}
         ></input>
-        <button type='submit' className={styles.button}>
+        <button
+          type='submit'
+          className={styles.button}
+          onClick={handleSendMessage}
+        >
           작성 완료
         </button>
       </form>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/server/write',
+        destination: 'http://localhost:8000/bestline',
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
