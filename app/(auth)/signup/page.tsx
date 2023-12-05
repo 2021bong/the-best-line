6fb7816db256f8007fd8955dc19e40e4bec7defd
Bong <2021bongwonny@gmail.com>
2023-12-05 23:48:25 +0900

    refactor : seperating code that initializing firebase app

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { initializeApp } from 'firebase/app';
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -7,17 +6,7 @@ import {
 } from 'firebase/auth';
 import styles from './page.module.scss';
 import { useState } from 'react';
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_APPKEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
-};
-const app = initializeApp(firebaseConfig);
+import app from '@/util/initializeFirebase';
 
 export default function Login() {
   const [email, setEmail] = useState('');
